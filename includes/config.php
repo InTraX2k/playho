b1316b92da694493e8638a9c088fc636692ea8d4
InTraX2k <Danielwelschmail@gmail.com>
2017-03-30 10:36:59 +0200

    Upload

---
+++ includes/config.php
@@ -0,0 +1,14 @@
+<?php
+//### Database access ###//
+
+$database					= array();
+$database['host']			= 'localhost';
+$database['port']			= '3306';
+$database['user']			= 'btgv5';
+$database['userpw']			= 'Series87!';
+$database['databasename']	= 'btgv5';
+$database['tableprefix']	= 'uni1_';
+$salt						= '/o.nG7tN2JW0ylU5eFmuZV'; // 22 digits from the alphabet "./0-9A-Za-z"
+
+//### Do not change beyond here ###//
+?>
