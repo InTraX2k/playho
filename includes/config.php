a35bac74cb8622d234eb4ee19026c0cc76c9e8d2
InTraX2k <Danielwelschmail@gmail.com>
2017-12-28 12:49:29 +0100

    update rev.

--- includes/config.php
+++ includes/config.php
@@ -4,9 +4,9 @@
 $database					= array();
 $database['host']			= 'localhost';
 $database['port']			= '3306';
-$database['user']			= 'btgv5';
-$database['userpw']			= 'Series87!';
-$database['databasename']	= 'btgv5';
+$database['user']			= '******';
+$database['userpw']			= '******!';
+$database['databasename']	= '******';
 $database['tableprefix']	= 'uni1_';
 $salt						= '/o.nG7tN2JW0ylU5eFmuZV'; // 22 digits from the alphabet "./0-9A-Za-z"
 
